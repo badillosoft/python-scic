57a6a4404546df414e8dcef8249dd6a445919b6e
Alan Badillo <badillo.soft@hotmail.com>
2017-12-09 12:47:57 -0600

    Sesión 7

--- practicas/opencv.md
+++ practicas/opencv.md
@@ -1,3 +1,5 @@
+## Windows
+
 > Tutorial completo: https://www.solarianprogrammer.com/2016/09/17/install-opencv-3-with-python-3-on-windows/
 
 > Numpy: http://www.lfd.uci.edu/~gohlke/pythonlibs/#numpy
@@ -5,3 +7,7 @@
 > OpenCV: http://www.lfd.uci.edu/~gohlke/pythonlibs/#opencv
 
 > Instalar: `pip install XXXX.whl`
+
+## OSX
+
+> `git clone https://github.com/Itseez/opencv.git`
---
+++ sesion_7/twitter.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+import tweepy
+
+consumer_key = "c3lj0ZqXCOPGYEvzr7vVsYpHK"
+consumer_secret = "1O3ajogwsogND8KluAWiYB9FIYmM7ytXZSgTmhoXz1A7bI2YqB"
+access_token = "64889616-a905C5DYOhrmS3ZVZKdkaU3Gl3fS1lJbQmSLciuWZ"
+access_token_secret = "lVofVI18n2rf9NyeZzaE9f75vjjw0lkkEIdpGuSUMA8Pf"
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth)
+
+tweets = api.home_timeline()
+
+for tweet in tweets:
+    print(tweet.text)
+
+# users = api.search_users("coca-cola")
+
+# for user in users:
+#     print(user)
